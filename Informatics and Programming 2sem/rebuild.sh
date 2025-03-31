08d6497ccf6dd889d5d31558ea4db9c7083f0920
gohy <gohy2006@mail.ru>
2025-03-31 22:48:51 +0400

    refactor:  build .cpp files from InfAndProg folder and write execs into .gitignore with rebuild.sh

--- Informatics and Programming 2sem/rebuild.sh
+++ Informatics and Programming 2sem/rebuild.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-find . -name $1 | entr clang++ -fstandalone-debug --debug $1 -o $(echo "$1" | cut -f 1 -d '.')
+path1=$(echo "${1%.*}")
+dir=$(pwd)
+input="${dir%/}"
+result=$(basename "$input")
+path2="${result}"/"${path1}"
+gitignore="../.gitignore"
+if ! grep -qxF "$path2" "$gitignore"; then
+  (echo "$path2") >> $gitignore
+fi
+
+echo "$1" | entr clang++ -fstandalone-debug --debug "$1" -o "$path1"
