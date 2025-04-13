5932b7bb371b5cfbb732a1cc988e8241a622eb8b
gohy <gohy2006@mail.ru>
2025-04-13 20:27:16 +0400

    build(rebuild.sh): update rebuild.sh from main branch

--- Informatics and Programming 2sem/rebuild.sh
+++ Informatics and Programming 2sem/rebuild.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 
 path1=$(echo "${1%.*}")
-dir=$(pwd)
-input="${dir%/}"
-result=$(basename "$input")
-path2="${result}"/"${path1}"
-gitignore="../.gitignore"
-if ! grep -qxF "$path2" "$gitignore"; then
-  (echo "$path2") >> $gitignore
-fi
-
 echo "$1" | entr clang++ -fstandalone-debug --debug "$1" -o "$path1"
