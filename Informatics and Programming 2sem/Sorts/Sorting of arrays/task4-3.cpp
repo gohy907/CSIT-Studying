0540c0cccb78999f764b50461660e38335869304
gohy <gohy2006@mail.ru>
2025-04-17 11:38:11 +0400

    docs(Sorting of arrays): rename files according to problem they solve

--- Informatics and Programming 2sem/Sorts/Sorting of arrays/task4-3.cpp
+++ Informatics and Programming 2sem/Sorts/Sorting of arrays/task4-3.cpp
@@ -1,8 +1,61 @@
+#include <algorithm>
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <math.h>
+#include <pthread.h>
 #include <sstream>
 #include <vector>
+
+void LSD_sort(std::vector<int> &vec) {
+  int k = 1;
+  for (int i = 0; i < vec.size(); ++i) {
+    int size = floor(log10(vec[i]) + 1);
+    if (size > k) {
+      k = size;
+    }
+  }
+  int p = 10;
+  std::vector<std::vector<int>> vecs(p);
+
+  for (int i = 0; i < k; ++i) {
+    for (int j = 0; j < vec.size(); ++j) {
+      int m = vec[j] / pow(p, i);
+      vecs[m % p].push_back(vec[j]);
+    }
+
+    int l = 0;
+    for (int j = 0; j < p; ++j) {
+      for (int h = 0; h < vecs[j].size(); ++h, ++l) {
+        vec[l] = vecs[j][h];
+      }
+    }
+
+    for (int j = 0; j < p; ++j) {
+      vecs[j].clear();
+    }
+  }
+}
+
+void transpose(std::map<int, std::vector<int>> &matrix) {
+  std::map<int, std::vector<int>> matrix2;
+  for (int i = 0; i < matrix[i].size(); ++i) {
+    std::vector<int> vec;
+    for (int j = 0; j < matrix[i].size(); ++j) {
+      vec.push_back(0);
+    }
+    matrix2[i] = vec;
+  }
+
+  for (int i = 0; i < matrix[i].size(); ++i) {
+    for (int j = 0; j < matrix[i].size(); ++j) {
+      matrix2[j][i] = matrix[i][j];
+    }
+  }
+
+  matrix = matrix2;
+}
 
 void readFrom(std::map<int, std::vector<int>> &matrix, std::string file) {
   std::ifstream in(file);
@@ -33,45 +86,15 @@ void writeTo(std::map<int, std::vector<int>> &matrix, std::string file) {
   }
 }
 
-void insertionSort(std::vector<int> &vec) {
-  int left = 0;
-  int right = vec.size();
-  for (int i = left; i < right; ++i) {
-    for (int j = i - 1; j >= left; --j) {
-      if (vec[j] > vec[j + 1]) {
-        int tmp = vec[j + 1];
-        vec[j + 1] = vec[j];
-        vec[j] = tmp;
-      } else {
-        break;
-      }
-    }
-  }
-}
-
 int main() {
+  srand(time(0));
   std::map<int, std::vector<int>> matrix;
   readFrom(matrix, "input.txt");
-
-  int n = matrix[0].size();
-  for (int k = 1; k < 2 * n - 2; ++k) {
-    std::vector<int> vec;
-    for (int i = 0; i < n; ++i) {
-      if (k - i < n && k - i >= 0) {
-        vec.push_back(matrix[i][k - i]);
-      }
-    }
-
-    insertionSort(vec);
-
-    int l = 0;
-    for (int i = 0; i < n; ++i) {
-      if (k - i < n && k - i >= 0) {
-        matrix[i][k - i] = vec[l];
-        ++l;
-      }
-    }
+  transpose(matrix);
+  for (int i = 0; i < matrix[0].size(); ++i) {
+    LSD_sort(matrix[i]);
+    std::reverse(matrix[i].begin(), matrix[i].end());
   }
-
+  transpose(matrix);
   writeTo(matrix, "output.txt");
 }
