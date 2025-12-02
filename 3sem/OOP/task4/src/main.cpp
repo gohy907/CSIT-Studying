b827e5b98cffd56720706d88a0aac417663368b8
gohy279 <gohy20062@mail.ru>
2025-12-02 18:57:05 +0400

    feat: initialize cyrillic font

--- 3sem/OOP/task4/src/main.cpp
+++ 3sem/OOP/task4/src/main.cpp
@@ -14,18 +14,35 @@ const Rectangle GREEN_CAR_SPRITE = Rectangle{(CAR_WIDTH + 1), 0, CAR_WIDTH, CAR_
 const Rectangle YELLOW_CAR_SPRITE = Rectangle{(CAR_WIDTH + 1) * 2, 0, CAR_WIDTH, CAR_HEIGHT};
 const Rectangle BLUE_CAR_SPRITE = Rectangle{(CAR_WIDTH + 1) * 3, 0, CAR_WIDTH, CAR_HEIGHT};
 
+Font InitRussianFont(const char *fontPath, int fontSize) {
+    int charsCount = 0;
+    int* chars = LoadCodepoints(
+        "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+        "0123456789"
+        ".,!?-+()[]{}:;/\\\"'`~@#$%^&*=_|<> "
+        "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm", 
+        &charsCount
+    );
+
+    Font font = LoadFontEx(fontPath, fontSize, chars, charsCount);
+    UnloadCodepoints(chars);
+    return font;
+}
+
 int main()
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - basic window");
 
+    Font font = InitRussianFont("static/HYWenHei.ttf", 50);
     Road mainRoad = Road(150, 100.0, 1600.0, 150.0, 5.0, BLACK, BLACK, RAYWHITE);
-    Texture2D atlas = LoadTexture("./cars.png");
+    Texture2D atlas = LoadTexture("static/cars.png");
     Car car = Car(Vector2{0, 130}, Vector2{10, 0},  BLUE_CAR_SPRITE, atlas);
     SetTargetFPS(60);
     mainRoad.addCar(car);
     while (!WindowShouldClose())
     {
         BeginDrawing();
+            DrawTextEx(font, "aboba", Vector2{100.0, 300.0}, 50, 0, BLACK);
             mainRoad.draw();
             mainRoad.update();
             ClearBackground(RAYWHITE);
