8163e1e3b93a3240e6fc51fb85945a3186418ba0
gohy279 <gohy20062@mail.ru>
2025-12-15 16:40:29 +0400

    feat(ASM/Lab8): make calculator

--- 3sem/ASM/Labs/Lab8/1.asm
+++ 3sem/ASM/Labs/Lab8/1.asm
@@ -11,14 +11,12 @@ STD_OUTPUT_HANDLE = -11
 STD_INPUT_HANDLE  = -10
 hStdInput qword ?
 hStdOutput qword ?
-sum dw ?
 aInput db 'a = ', 0
-a db ' ', 0
 bInput db 'b = ', 0 
-b db ?
-abInput db 'a + b = ', 0 
+output db 'a + b = ', 0 
 invalidChar db 'Invalid character', 0 
 pressAnyKey db 'Press any key to exit...', 0 
+newLine db 0Dh, 0Ah, 0
 
 STACKALLOC macro arg
     push R15
@@ -41,6 +39,13 @@ endm
 
 .code
 
+PrintNL proc 
+    lea RAX, newLine
+    push RAX
+    call PrintString
+    ret
+PrintNL endp
+
 ; адрес строки должен находиться в RAX
 PrintString proc uses RAX RCX RDX R8 R9, string: QWORD  
     local bytesWritten: qword 
@@ -60,6 +65,126 @@ PrintString proc uses RAX RCX RDX R8 R9, string: QWORD
     ret 8
 PrintString endp
 
+ReadString proc uses RBX RCX RDX R8 R9
+    local ReadStr[64]: byte,
+          bytesRead: dword
+    STACKALLOC 2
+    mov RCX, hStdInput
+    lea RDX, readStr
+    mov R8, 64
+    lea R9, bytesRead
+    NULL_FIFTH_ARG
+    call ReadConsoleA
+    xor RCX, RCX
+    mov ECX, bytesRead
+    sub ECX, 2
+
+    xor RBX, RBX
+    mov R8, 1
+
+    stringloop:
+        dec RCX
+        cmp RCX, -1
+        je scanningComplete
+
+        xor RAX, RAX
+        mov AL, readStr[RCX]
+        cmp AL, '-'
+        jne eval
+        neg RBX
+
+        cmp RCX, 0 
+        je scanningComplete 
+        jmp error
+
+    eval:
+        cmp AL, 30h
+        jl error
+        cmp AL, 39h
+        jg error
+
+        sub RAX, 30h
+        mul R8
+        add RBX, RAX
+        mov RAX, 10
+        mul R8
+        mov R8, RAX
+        jmp stringloop
+        
+
+
+    scanningComplete:
+        mov R10, 0
+        mov RAX, RBX
+        STACKFREE
+        ret
+
+
+    error:
+        mov R10, 1
+        STACKFREE
+        ret
+
+ReadString endp
+
+PrintNumber proc uses RAX RCX RDX R8 R9 R10 R11, num: QWORD 
+    local numberStr[22]: byte 
+    STACKALLOC 1
+    xor R8, R8 
+    mov RAX, num
+    cmp num, 0
+    jge positive
+    mov numberStr[R8], '-'
+    inc R8
+    neg RAX
+
+    positive:
+    mov RBX, 10
+    xor RCX, RCX
+
+    divisionloop:
+        xor RDX, RDX
+        div RBX
+        add RDX, 30h
+        push RDX
+        inc RCX
+        cmp RAX, 0 
+        jne divisionloop 
+
+    stack:
+        pop RDX 
+        mov numberStr[R8], DL 
+        inc R8 
+        loop stack 
+
+    mov numberStr[R8], 0 
+    lea RAX, numberStr
+    push RAX 
+    call PrintString 
+    STACKFREE
+    ret 8 
+PrintNumber endp
+
+waitingForInput proc uses RAX RCX RDX R8 R9 R10 R11 
+    local readStr: byte, bytesRead: dword
+    STACKALLOC 1
+
+    lea RAX, pressAnyKey
+    push RAX
+    call PrintString
+
+    mov RCX, hStdInput
+    lea RDX, readStr
+    mov R8, 1
+    lea R9, bytesRead
+    NULL_FIFTH_ARG
+    call ReadConsoleA
+
+    STACKFREE
+    ret
+
+waitingForInput endp
+
 mainCRTStartup proc
     STACKALLOC
 
@@ -76,34 +201,42 @@ mainCRTStartup proc
 
 
     lea RAX, aInput
-    STACKALLOC
-    mov [RSP], RAX
+    push RAX
     call PrintString
-    STACKFREE
 
+    call ReadString
+    cmp R10, 1
+    je incorrect
+    mov RBX, RAX
 
     lea RAX, bInput
-    STACKALLOC
-    mov [RSP], RAX
+    push RAX
     call PrintString
-    STACKFREE
 
-    ; lea RAX, a
-    ; call inputStr
+    call ReadString
+    cmp R10, 1
+    je incorrect
+    jne correct
 
-    ; lea RAX, a
-    ; call printStr
-    ;
-    ; mov RCX, STD_OUTPUT_HANDLE
-    ; call GetStdHandle
-    ; mov hStdOutput, RAX
-    ;
-    ; mov RCX, hStdInput
-    ; lea RDX, input
-    ; mov R8D, 255
-    ; lea R9, read
-    ; mov qword ptr [rsp+32], 0
-    ; call ReadConsoleA
+    incorrect:
+        lea RAX, invalidChar
+        push RAX 
+        call PrintString
+        jmp exit
+
+    correct:
+        push RAX 
+        lea RAX, output 
+        push RAX
+        call PrintString
+        pop RAX
+        add RAX, RBX
+        push RAX 
+        call PrintNumber
+
+    exit: 
+    call PrintNL
+    call waitingForInput
     call ExitProcess
 mainCRTStartup endp
 end
