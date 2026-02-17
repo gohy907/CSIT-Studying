648c1b8cd70b7384667145ca37b3100eaddcde27
gohy907 <gohy2006@mail.ru>
2026-02-17 20:08:41 +0400

    fix(4sem/ACA): fix some things in counting sort

--- 4sem/ACA/Sorts/counting_sort.cpp
+++ 4sem/ACA/Sorts/counting_sort.cpp
@@ -16,27 +16,23 @@ void counting_sort(std::vector<int> &numbers) {
         }
     }
 
-    size_t range = static_cast<size_t>(max) - static_cast<size_t>(min) + 1;
-    std::vector<int> indices(range, 0);
+    size_t range = max - min + 1;
+    std::vector<size_t> indices(range, 0);
 
-    std::cout << "1" << std::endl;
     for (size_t i = 0; i < numbers.size(); ++i) {
-        size_t num = numbers[i];
+        int num = numbers[i];
         ++indices[num - min];
     }
 
-    std::cout << "2" << std::endl;
     size_t index = 0;
     for (int i = min; i <= max; ++i) {
-        size_t a = static_cast<size_t>(i) - static_cast<size_t>(min);
+        size_t a = i - min;
         int count = indices[a];
         for (int j = 0; j < count; ++j) {
             numbers[index] = i;
             ++index;
         }
     }
-
-    std::cout << "3" << std::endl;
 }
 
 void print_vector(std::vector<int> &numbers) {
@@ -46,21 +42,14 @@ void print_vector(std::vector<int> &numbers) {
     std::cout << std::endl;
 }
 
-#include "tester.hpp"
 int main() {
-    std::size_t length = 3;
-    // std::cout << "Введите длину массива: ";
-    // std::cin >> length;
+    std::size_t length;
+    std::cout << "Введите длину массива: ";
+    std::cin >> length;
 
-    // std::vector<int> numbers(length);
-    // std::cout << "Вводите числа:" << std::endl;
-    // for (std::size_t i = 0; i < length; ++i) {
-    //     std::cin >> numbers[i];
-    // }
-    // numbers.push_back(INT_MAX);
-    // numbers.push_back(INT_MIN);
-    // counting_sort(numbers);
-    // print_vector(numbers);
-    tester::Tester t;
-    t.test(counting_sort);
+    std::vector<int> numbers(length);
+    std::cout << "Вводите числа:" << std::endl;
+    for (std::size_t i = 0; i < length; ++i) {
+        std::cin >> numbers[i];
+    }
 }
