f67f7f795c0f35dde89f679a7f25f349854fbd8a
gohy907 <gohy2006@mail.ru>
2026-05-15 08:37:17 +0400

    feat(ACA): improve boyer-moure to O(n + m)

--- 4sem/ACA/Strings/boyer_moure.cpp
+++ 4sem/ACA/Strings/boyer_moure.cpp
@@ -39,8 +39,8 @@ std::map<char, int> compute_badchar(std::string &pattern) {
 }
 
 std::vector<size_t> compute_suffshift(std::string &s) {
-    size_t m = s.length();
-    std::vector<size_t> suffshift(m + 1, m);
+    size_t n = s.length();
+    std::vector<size_t> suffshift(n + 1, n);
 
     std::string reverse_s = s;
     reverse(reverse_s.begin(), reverse_s.end());
@@ -48,21 +48,21 @@ std::vector<size_t> compute_suffshift(std::string &s) {
     std::vector<size_t> z = z_func(reverse_s);
     reverse(z.begin(), z.end());
 
-    for (size_t i = 0; i < m - 1; ++i) {
+    for (size_t i = 0; i < n - 1; ++i) {
         size_t len = z[i];
         if (len > 0) {
-            suffshift[m - 1 - len] = m - 1 - i;
+            suffshift[n - 1 - len] = n - 1 - i;
         }
     }
 
     size_t j = 0;
-    for (size_t k = m; k > 0; --k) {
+    for (size_t k = n; k > 0; --k) {
         size_t i_idx = k - 1;
 
         if (z[i_idx] == i_idx + 1) {
-            size_t shift = m - 1 - i_idx;
+            size_t shift = n - 1 - i_idx;
             while (j <= shift) {
-                if (suffshift[j] == m) {
+                if (suffshift[j] == n) {
                     suffshift[j] = shift;
                 }
                 j++;
@@ -70,13 +70,13 @@ std::vector<size_t> compute_suffshift(std::string &s) {
         }
     }
 
-    while (j < m) {
-        if (suffshift[j] == m) {
-            suffshift[j] = m;
+    while (j < n) {
+        if (suffshift[j] == n) {
+            suffshift[j] = n;
         }
         j++;
     }
-    suffshift[m - 1] = 1;
+    suffshift[n - 1] = 1;
     return suffshift;
 }
 
@@ -88,16 +88,29 @@ void boyer_moure(std::string &input_string, std::string &substring) {
     std::map<char, int> badchar = compute_badchar(substring);
 
     int i = 0;
+    int last_s = -1;
+    int last_m = 0;
+
     while (i <= n - m) {
         int j = m - 1;
+        int stop_index;
+        if (i == last_s) {
+            stop_index = last_m - 1;
+        } else {
+            stop_index = -1;
+        }
 
-        while (j >= 0 && substring[j] == input_string[i + j]) {
+        while (j > stop_index && substring[j] == input_string[i + j]) {
             --j;
         }
 
-        if (j < 0) {
+        if (j <= stop_index) {
             std::cout << i << std::endl;
+            last_s = i + suffshift[0];
+            last_m = m - suffshift[0];
+
             i += suffshift[0];
+
         } else {
             char mismatched_char = input_string[i + j];
             int bad_char_shift;
@@ -110,6 +123,9 @@ void boyer_moure(std::string &input_string, std::string &substring) {
             int good_suffix_shift = suffshift[j];
 
             i += std::max(good_suffix_shift, bad_char_shift);
+
+            last_s = -1;
+            last_m = 0;
         }
     }
 }
